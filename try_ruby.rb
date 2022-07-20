4e96c7b5a86aede25bf0723fdedce465eb3c8f05
DESKTOP-RR8VSI4\barnet <shinya.umd@gmail.com>
2022-07-20 15:50:18 +0900

    初回テストプログラム

---
+++ try_ruby.rb
@@ -0,0 +1,15 @@
+=begin
+参考URL　https://qiita.com/masakinihirota/items/ec90086bab86f369fa15
+=end
+
+class Hello
+  def hello
+    puts 'HELLO'
+  end
+end
+
+hl = Hello.new
+hl.hello
+
+p 'p'
+puts 'puts'
